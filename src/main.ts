498723e7eda850a326f62805f9eb97fdc0668a87
Thor-BadgerBite <t.demis@gmail.com>
2022-08-31 09:55:30 +0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+enableProdMode()
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
