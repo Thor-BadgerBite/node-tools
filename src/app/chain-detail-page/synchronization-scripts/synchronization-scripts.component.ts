2bb675d91cca74127007790cb412fdb53d82b824
Thor-BadgerBite <t.demis@gmail.com>
2022-08-31 19:12:40 +0300

    2 files modified

--- src/app/chain-detail-page/synchronization-scripts/synchronization-scripts.component.ts
+++ src/app/chain-detail-page/synchronization-scripts/synchronization-scripts.component.ts
@@ -29,37 +29,37 @@ export class SynchronizationScriptsComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
-    this.chain = this.chainService.activeChain;
-    if (this.chain) {
-      if (!this.chain.isArchive) {
-        this.chainService.getChainNetInfo(this.chain)
-          .subscribe((data: any) => {
-              this.livePeers.push(this.chain?.rpcPeer || '');
-              let peersArray = data.result.peers;
-              for (let i = 0; i < peersArray.length && this.livePeers.length < this.MAX_LIVE_PEERS; i++) {
-                const peerId = peersArray[i].node_info.id;
-                const listenAddr = peersArray[i].node_info.listen_addr;
-                const listenPort = listenAddr.slice(listenAddr.lastIndexOf(':') + 1);
-                const remoteIp = peersArray[i].remote_ip;
-                if (this.utilsService.isIP6Address(remoteIp)) {
-                  this.livePeers.push(`${peerId}@[${remoteIp}]:${listenPort}`);
-                } else {
-                  this.livePeers.push(`${peerId}@${remoteIp}:${listenPort}`);
-                }
-              }
-              this.updateLivePeersView();
-            }
-          );
-      }
-      this.chainService.getChainSnapshotInfo(this.chain)
-        .subscribe((data: any) => {
-          const snapshotHeight = data.snapshotHeight;
-          const snapshotSize = data.snapshotSize + 'B';
-          const snapshotBlockTime = this.utilsService.humanReadableTimeDifferenceString(data.snapshotBlockTime);
-          this.snapshotData = new SnapshotData(snapshotHeight, snapshotSize, snapshotBlockTime);
-        });
-    }
-    this.highlightService.highlightAll(this.highlightRPCLink.bind(this));
+    // this.chain = this.chainService.activeChain;
+    // if (this.chain) {
+    //   if (!this.chain.isArchive) {
+    //     this.chainService.getChainNetInfo(this.chain)
+    //       .subscribe((data: any) => {
+    //           this.livePeers.push(this.chain?.rpcPeer || '');
+    //           let peersArray = data.result.peers;
+    //           for (let i = 0; i < peersArray.length && this.livePeers.length < this.MAX_LIVE_PEERS; i++) {
+    //             const peerId = peersArray[i].node_info.id;
+    //             const listenAddr = peersArray[i].node_info.listen_addr;
+    //             const listenPort = listenAddr.slice(listenAddr.lastIndexOf(':') + 1);
+    //             const remoteIp = peersArray[i].remote_ip;
+    //             if (this.utilsService.isIP6Address(remoteIp)) {
+    //               this.livePeers.push(`${peerId}@[${remoteIp}]:${listenPort}`);
+    //             } else {
+    //               this.livePeers.push(`${peerId}@${remoteIp}:${listenPort}`);
+    //             }
+    //           }
+    //           this.updateLivePeersView();
+    //         }
+    //       );
+    //   }
+    //   this.chainService.getChainSnapshotInfo(this.chain)
+    //     .subscribe((data: any) => {
+    //       const snapshotHeight = data.snapshotHeight;
+    //       const snapshotSize = data.snapshotSize + 'B';
+    //       const snapshotBlockTime = this.utilsService.humanReadableTimeDifferenceString(data.snapshotBlockTime);
+    //       this.snapshotData = new SnapshotData(snapshotHeight, snapshotSize, snapshotBlockTime);
+    //     });
+    // }
+    // this.highlightService.highlightAll(this.highlightRPCLink.bind(this));
   }
 
   updateLivePeersView(): void {
