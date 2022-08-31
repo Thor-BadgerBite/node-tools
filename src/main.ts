6adcd8b11a19a93a2a224cf215bdfa53db00ab12
Thor-BadgerBite <t.demis@gmail.com>
2022-08-31 19:33:42 +0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import './polyfills';
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-enableProdMode()
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+import { environment } from './environments/environment';
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
